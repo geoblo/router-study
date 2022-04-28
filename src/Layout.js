e795307580890f3620ba27cb95ad5d75d02315a0
klina8575 <klina8575@gmail.com>
2022-04-28 16:16:55 +0900

    up

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function Layout() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1); //이전 페이지 이동
+  };
+  const goArticles = () => {
+    navigate("/articles");
+  };
+
   return (
     <div>
       <header style={{ background: "gray", padding: 16, fontSize: 24 }}>
-        header
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}>게시글 목록</button>
       </header>
       <main>
         <Outlet />
