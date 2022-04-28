48f2c03011a5246eeff6c35809ae719b2e2980b0
klina8575 <klina8575@gmail.com>
2022-04-28 15:35:33 +0900

    up

---
+++ src/pages/Articles.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Articles() {
+  return (
+    <ul>
+      <li>
+        <Link to="/articles/1">게시글1</Link>
+      </li>
+      <li>
+        <Link to="/articles/2">게시글2</Link>
+      </li>
+      <li>
+        <Link to="/articles/3">게시글3</Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Articles;
